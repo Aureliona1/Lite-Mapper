f3ac1977085f7a14117be85cda511cd9a3ef7747
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2023-10-17 00:20:18 +1030

    Basic Object Classes

--- src.ts
+++ src.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-explicit-any
 type DiffTypes<T extends string> = `Easy${T}` | `Normal${T}` | `Hard${T}` | `Expert${T}` | `ExpertPlus${T}`;
 export type DiffNames = DiffTypes<"Standard"> | DiffTypes<"Lightshow"> | DiffTypes<"Lawless">;
 type FilterObject = { c: number; f: number; p: number; t: number; r: number; n: number; s: number; l: number; d: number };
@@ -12,9 +13,8 @@ export type KFSingle = [number, number, Easing?];
 export type KFVec4 = [number, number, number, number, number, Easing?, "splineCatmullRom"?];
 type GeometryObjectTypes = "Sphere" | "Capsule" | "Cylinder" | "Cube" | "Plane" | "Quad" | "Triangle";
 export type MaterialShader = "Standard" | "OpaqueLight" | "TransparentLight" | "BaseWater" | "BillieWater" | "BTSPillar" | "InterscopeConcrete" | "InterscopeCar" | "Obstacle" | "WaterfallMirror";
-export type GeometryMaterialJSON = { shader: MaterialShader; color?: Vec3 | Vec4; track?: string; shaderKeywords: string[] };
+export type GeometryMaterialJSON = { shader: MaterialShader; color?: Vec3 | Vec4; track?: string; shaderKeywords?: string[] };
 export type GeometryObjectJSON = { type: GeometryObjectTypes; material: GeometryMaterialJSON | string };
-export type EnvironmentObjectJSON = { id?: string; lookupMethod?: LookupMethod; duplicate?: number; active?: boolean; scale?: Vec3; position?: Vec3; localPosition?: Vec3; rotation?: Vec3; localRotation?: Vec3; track?: string | string[]; geometry?: GeometryObjectJSON };
 type CustomEventNames = "AnimateTrack" | "AssignPathAnimation" | "AssignTrackParent" | "AssignPlayerToTrck" | "AnimateComponent";
 export type ObjectAnimProps = {
 	offsetPosition?: Vec3 | KFVec3[];
@@ -92,23 +92,28 @@ type NoteCustomProps = {
 };
 type SliderCustomProps = { coordinates?: Vec2; worldRotation?: Vec3; localRotation?: Vec3; noteJumpMovementSpeed?: number; noteJumpStartBeatOffset?: number; uninteractable?: boolean; disableNoteGravity?: boolean; tailCoordinates?: Vec2; color?: Vec3 | Vec4; animation?: ObjectAnimProps };
 
+type NoteType = { b: number; x: number; y: number; c: number; d: number; a: number; customData?: NoteCustomProps };
+type BombType = { b: number; x: number; y: number; customData?: NoteCustomProps };
+type ObstacleType = {
+	b: number;
+	x: number;
+	y: number;
+	d: number;
+	w: number;
+	h: number;
+	customData?: { size?: Vec3; animation?: ObjectAnimProps; coordinates?: Vec2; worldRotation?: Vec3; localRotation?: Vec3; noteJumpMovementSpeed?: number; noteJumpStartBeatOffset?: number; uninteractable?: boolean; color?: Vec3 | Vec4 };
+};
+type BurstSliderType = { b: number; x: number; y: number; c: number; d: number; tb: number; tx: number; ty: number; sc: number; s: number; customData?: SliderCustomProps };
+
 export type RawMapJSON = {
-	version: "3.2.0";
+	version: string;
 	bpmEvents: { b: number; m: number }[];
 	rotationEvents: { b: number; e: number; r: number }[];
-	colorNotes: { b: number; x: number; y: number; c: number; d: number; a: number; customData?: NoteCustomProps }[];
-	bombNotes: { b: number; x: number; y: number; customData?: NoteCustomProps }[];
-	obstacles: {
-		b: number;
-		x: number;
-		y: number;
-		d: number;
-		w: number;
-		h: number;
-		customData?: { size?: Vec3; animation?: ObjectAnimProps; coordinates?: Vec2; worldRotation?: Vec3; localRotation?: Vec3; noteJumpMovementSpeed?: number; noteJumpStartBeatOffset?: number; uninteractable?: boolean; color?: Vec3 | Vec4 };
-	}[];
+	colorNotes: NoteType[];
+	bombNotes: BombType[];
+	obstacles: ObstacleType[];
 	sliders: { b: number; c: number; x: number; y: number; d: number; mu: number; tb: number; tx: number; ty: number; tc: number; tmu: number; m: number; customData?: SliderCustomProps }[];
-	burstSliders: { b: number; x: number; y: number; c: number; d: number; tb: number; tx: number; ty: number; sc: number; s: number; customData?: SliderCustomProps }[];
+	burstSliders: BurstSliderType[];
 	waypoints: [];
 	basicBeatmapEvents: {
 		b: number;
@@ -121,13 +126,21 @@ export type RawMapJSON = {
 	lightColorEventBoxGroups: { b: number; g: number; e: { f: FilterObject; w: number; d: number; r: number; t: number; b: number; i: number; e: { b: number; i: number; c: number; s: number; f: number }[] }[] }[];
 	lightRotationEventBoxGroups: { b: number; g: number; e: { f: FilterObject; w: number; d: number; s: number; t: number; b: number; i: number; a: number; r: number; l: { b: number; p: number; e: number; l: number; r: number; o: number }[] }[] }[];
 	lightTranslationEventBoxGroups: { b: number; g: number; e: { f: FilterObject; w: number; d: number; s: number; t: number; b: number; i: number; a: number; r: number; l: { b: number; p: number; e: number; t: number }[] }[] }[];
-	basicEventTypesWithKeywords: {};
+	basicEventTypesWithKeywords: Record<any, any>;
 	useNormalEventsAsCompatibleEvents: boolean;
-	customData?: { customEvents?: { b: number; t: CustomEventNames; d: TrackAnimProps | PathAnimProps | TrackParentProps | PlayerToTrackProps | ComponentAnimProps }[]; environment?: EnvironmentObjectJSON[]; materials?: Record<any, GeometryMaterialJSON> };
+	customData?: {
+		customEvents?: { b: number; t: CustomEventNames; d: TrackAnimProps | PathAnimProps | TrackParentProps | PlayerToTrackProps | ComponentAnimProps }[];
+		environment?: (Environment | Geometry)[];
+		materials?: Record<any, GeometryMaterialJSON>;
+		fakeColorNotes?: NoteType[];
+		fakeBombNotes?: BombType[];
+		fakeObstacles?: ObstacleType[];
+		fakeBurstSliders?: BurstSliderType[];
+	};
 };
 
 export class BeatMap {
-	private map: Record<any, any> = {
+	private map: RawMapJSON = {
 		version: "3.2.0",
 		bpmEvents: [],
 		rotationEvents: [],
@@ -146,5 +159,85 @@ export class BeatMap {
 		useNormalEventsAsCompatibleEvents: false,
 		customData: {}
 	};
-	constructor(public readonly inputDiff: DiffNames = "ExpertStandard", public readonly outputDiff: DiffNames = "ExpertPlusStandard") {}
+	constructor(public readonly inputDiff: DiffNames = "ExpertStandard", public readonly outputDiff: DiffNames = "ExpertPlusStandard") {
+		this.map = JSON.parse(Deno.readTextFileSync(inputDiff + ".dat"));
+	}
+
+	public readonly version = this.map.version;
+	public bpmEvents = this.map.bpmEvents;
+	public rotationEvents = this.map.rotationEvents;
+	public notes = this.map.colorNotes;
+	public bombs = this.map.bombNotes;
+	public walls = this.map.obstacles;
+	public arcs = this.map.sliders;
+	public chains = this.map.burstSliders;
+	public events = this.map.basicBeatmapEvents;
+	public colorBoostBeatmapEvents = this.map.colorBoostBeatmapEvents;
+	public lightColorEventBoxGroups = this.map.lightColorEventBoxGroups;
+	public lightRotationEventBoxGroups = this.map.lightRotationEventBoxGroups;
+	public lightTranslationEventBoxGroups = this.map.lightTranslationEventBoxGroups;
+	public customData = this.map.customData;
+	public customEvents = this.map.customData?.customEvents;
+	public environments = this.map.customData?.environment;
+	public materials = this.map.customData?.materials;
+	public fakeNotes = this.map.customData?.fakeColorNotes;
+	public fakeBombs = this.map.customData?.fakeBombNotes;
+	public fakeObstacles = this.map.customData?.fakeObstacles;
+	public fakeChains = this.map.customData?.fakeBurstSliders;
+
+	save() {
+		Deno.writeTextFileSync(this.outputDiff + ".dat", JSON.stringify(this.map));
+	}
+}
+
+export class Environment {
+	constructor(public id: string, public lookupMethod: LookupMethod = "Contains") {}
+	public active?: boolean;
+	duplicate?: number;
+	scale?: Vec3;
+	position?: Vec3;
+	localPosition?: Vec3;
+	rotation?: Vec3;
+	localRotation?: Vec3;
+	track?: string | string[];
+	return() {
+		return this;
+	}
+}
+
+export class Geometry {
+	public geometry: GeometryObjectJSON = { type: "Cube", material: { shader: "Standard" } };
+	constructor(type?: GeometryObjectTypes, material?: GeometryMaterialJSON | string) {
+		if (type) {
+			this.geometry.type = type;
+		}
+		if (material) {
+			this.geometry.material = material;
+		}
+	}
+	public scale?: Vec3;
+	position?: Vec3;
+	localPosition?: Vec3;
+	rotation?: Vec3;
+	localRotation?: Vec3;
+	track?: string | string[];
+	return() {
+		return this;
+	}
+}
+
+export class Note {
+	constructor(public time = 0, public pos: Vec2 = [0, 0], public type = 0, public direction = 0, public angleOffset = 0) {}
+	public customData?: NoteCustomProps;
+	return(): NoteType {
+		return {
+			b: this.time,
+			x: this.pos[0],
+			y: this.pos[1],
+			c: this.type,
+			d: this.direction,
+			a: this.angleOffset,
+			customData: this.customData
+		};
+	}
 }
