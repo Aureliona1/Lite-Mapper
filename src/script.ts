146103071d083dbd3eccebe9fa93314f6c28dd37
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2023-10-18 19:41:44 +1030

    Default Project

---
+++ src/script.ts
@@ -0,0 +1,9 @@
+import { BeatMap } from "./LiteMapper.ts";
+
+const map = new BeatMap("ExpertStandard", "ExpertPlusStandard");
+
+// Put your map code below here...
+
+// Put your map code above here...
+
+map.save();
