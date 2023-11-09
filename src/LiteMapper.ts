9a49a38ee54c9425f281a3b02bf9989ac6940400
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-11-09 11:02:48 +1030

    Bidirectional Mapping

--- src/LiteMapper.ts
+++ src/LiteMapper.ts
@@ -72,37 +72,55 @@ export type ComponentAnimProps = {
 	TubeBloomPrePassLight?: { colorAlphaMultiplier?: number; bloomFogIntensityMultiplier?: number };
 };
 
-enum LightEventTypesEnum {
-	"BackLasers" = 0,
-	"RingLights" = 1,
-	"LeftLasers" = 2,
-	"RightLasers" = 3,
-	"CenterLights" = 4,
-	"BoostColors" = 5,
-	"RingSpin" = 8,
-	"RingZoom" = 9,
-	"LeftLaserSpeed" = 12,
-	"RightLaserSpeed" = 13
-}
-
 export type LightEventTypes = "BackLasers" | "RingLights" | "LeftLasers" | "RightLasers" | "CenterLights" | "BoostColors" | "RingSpin" | "RingZoom" | "LeftLaserSpeed" | "RightLaserSpeed";
 
-enum LightEventValuesEnum {
-	"Off" = 0,
-	"OnBlue" = 1,
-	"FlashBlue" = 2,
-	"FadeBlue" = 3,
-	"Transition" = 4,
-	"TransitionBlue" = 4,
-	"OnRed" = 5,
-	"FlashRed" = 6,
-	"FadeRed" = 7,
-	"TransitionRed" = 8,
-	"OnWhite" = 9,
-	"FlashWhite" = 10,
-	"FadeWhite" = 11,
-	"TransitionWhite" = 12
+class TwoWayMap {
+	private reverseMap: Record<any, any>;
+	constructor(private map: Record<any, any>) {
+		this.map = map;
+		this.reverseMap = {};
+		for (const key in map) {
+			const value = map[key];
+			this.reverseMap[value] = key;
+		}
+	}
+	get(key: any) {
+		return this.map[key];
+	}
+	revGet(key: any) {
+		return this.reverseMap[key];
+	}
 }
+
+const LightEventTypesMap = new TwoWayMap({
+	BackLasers: 0,
+	RingLights: 1,
+	LeftLasers: 2,
+	RightLasers: 3,
+	CenterLights: 4,
+	BoostColors: 5,
+	RingSpin: 8,
+	RingZoom: 9,
+	LeftLaserSpeed: 12,
+	RightLaserSpeed: 13
+});
+
+const LightEventValuesMap = new TwoWayMap({
+	Off: 0,
+	OnBlue: 1,
+	FlashBlue: 2,
+	FadeBlue: 3,
+	Transition: 4,
+	TransitionBlue: 4,
+	OnRed: 5,
+	FlashRed: 6,
+	FadeRed: 7,
+	TransitionRed: 8,
+	OnWhite: 9,
+	FlashWhite: 10,
+	FadeWhite: 11,
+	TransitionWhite: 12
+});
 
 export type LightEventValues = "Off" | "OnBlue" | "FlashBlue" | "FadeBlue" | "Transition" | "TransitionBlue" | "OnRed" | "FlashRed" | "FadeRed" | "TransitionRed" | "OnWhite" | "FlashWhite" | "FadeWhite" | "TransitionWhite";
 
@@ -1682,16 +1700,16 @@ export class LightEvent {
 		jsonPrune(this);
 		return {
 			b: this.time,
-			et: LightEventTypesEnum[this.type],
-			i: LightEventValuesEnum[this.value],
+			et: LightEventTypesMap.get(this.type),
+			i: LightEventValuesMap.get(this.value),
 			f: this.floatValue,
 			customData: this.customData
 		};
 	}
 	JSONToClass(x: LightEventType) {
 		this.time = x.b;
-		this.type = LightEventTypesEnum[x.et] as LightEventTypes;
-		this.value = LightEventValuesEnum[x.i] as LightEventValues;
+		this.type = LightEventTypesMap.revGet(x.et);
+		this.value = LightEventValuesMap.revGet(x.i);
 		if (x.customData) {
 			this.customData = x.customData;
 		}
