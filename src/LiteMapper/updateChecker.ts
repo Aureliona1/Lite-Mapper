8bc44b7562eac1e864acc0474fe5ff07184bbb3c
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-11-10 15:18:38 +1030

    Update updateChecker.ts

--- src/LiteMapper/updateChecker.ts
+++ src/LiteMapper/updateChecker.ts
@@ -4,7 +4,7 @@ import { LMLog } from "./LiteMapper.ts";
 export async function LMUpdateCheck() {
 	try {
 		const raw = (await fetch("https://raw.githubusercontent.com/Aureliona1/Lite-Mapper/main/src/LiteMapper/updateChecker.ts")).text(),
-			remoteVer = eval(await raw).ver as string,
+			remoteVer = eval(`${/".+";/.exec(await raw)}`),
 			localVer = ver;
 		if (localVer !== remoteVer) {
 			LMLog(`A new update of Lite-Mapper is available (${remoteVer}):\n    \x1b[94mhttps://github.com/Aureliona1/Lite-Mapper/releases/latest\x1b[37m`);
