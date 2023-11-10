29eef973a6b5ec95556c884a63546ba93569b71c
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-11-10 15:07:38 +1030

    This probably doesn't work

--- src/LiteMapper/updateChecker.ts
+++ src/LiteMapper/updateChecker.ts
@@ -1,11 +1,13 @@
+const ver = "beta-0.0.1";
 import { LMLog } from "./LiteMapper.ts";
 
 export async function LMUpdateCheck() {
 	try {
-		const raw = (await fetch("https://raw.githubusercontent.com/Aureliona1/Lite-Mapper/main/src/LiteMapper/LMVER.json")).json(),
-			ver = (await raw)["version"];
-		if (JSON.parse(Deno.readTextFileSync("LMVER.json"))["version"] !== ver) {
-			LMLog("A new update of Lite-Mapper is available:\n    \x1b[94mhttps://github.com/Aureliona1/Lite-Mapper/releases/latest\x1b[37m");
+		const raw = (await fetch("https://raw.githubusercontent.com/Aureliona1/Lite-Mapper/main/src/LiteMapper/updateChecker.ts")).text(),
+			remoteVer = eval(await raw).ver as string,
+			localVer = ver;
+		if (localVer !== remoteVer) {
+			LMLog(`A new update of Lite-Mapper is available (${remoteVer}):\n    \x1b[94mhttps://github.com/Aureliona1/Lite-Mapper/releases/latest\x1b[37m`);
 		}
 	} catch (e) {
 		LMLog(e, "Error");
