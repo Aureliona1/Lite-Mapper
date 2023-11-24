19e60cd59efa48effdc8bc60f5d49bb26e02ffd3
Aureliona <62498557+Aureliona1@users.noreply.github.com>
2023-11-24 20:25:32 +1030

    Only need 1

--- Lite-Mapper/src/Functions.ts
+++ Lite-Mapper/src/Functions.ts
@@ -253,26 +253,17 @@ export function arrFromFunction(length: number, func: (x: number) => number) {
 }
 
 /**
- * Random number generator with optional seed for reproducible results.
+ * Generate a random number.
  * @param min The minimun possible number to generate (inclusive).
  * @param max The maximum possible number to generate (exclusive).
  * @param seed The optional seed to apply to the generator (leave blank for random).
  * @param precision (Default - 3) The number of decimals in the random number. This can be negative to round to different values, e.g., -1 will round to the nearest 10, -2 will round to the nearest 100 etc.
  * @returns Random number.
  */
-export function seedRNG(min: number, max: number, seed: number | string = Date.now(), precision = 3) {
+export function random(min: number, max: number, seed: number | string = Date.now(), precision = 3) {
 	[min, max] = min > max ? [max, min] : [min, max];
 	return Math.round(new Seed(seed.toString()).randomFloat(min, max) * Math.pow(10, precision)) / Math.pow(10, precision);
 }
-
-/**
- * Generate a random number.
- * @param min The minimum possible number to generate (inclusive).
- * @param max The maximum possible number to generate (exclusive).
- * @param precision (Default - 3) The number of decimals in the random number. This can be negative to round to different values, e.g., -1 will round to the nearest 10, -2 will round to the nearest 100 etc.
- * @returns Random number.
- */
-export const random = (min: number, max: number, precision = 3) => Math.round((Math.random() * (max - min) + min) * Math.pow(10, precision)) / Math.pow(10, precision);
 
 /**
  * Finds the rotation of an object at point1 so that it faces point2.
