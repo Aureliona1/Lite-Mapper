745f12f102f59e4ae944e64c2fb382461c89c92a
Aurellis <62498557+Aureliona1@users.noreply.github.com>
2024-04-23 21:52:11 +0930

    Material presets

--- Lite-Mapper/src/Consts.ts
+++ Lite-Mapper/src/Consts.ts
@@ -1,4 +1,89 @@
-import { Vec3 } from "./Types.ts";
+import { GeometryMaterialJSON, Vec3 } from "./Types.ts";
 
 export const ye = -69420;
 export const ye3: Vec3 = [ye, ye, ye];
+
+/**
+ * Ported from https://github.com/Shonshyn/BS-Tools-for-Mappers/blob/main/Geo%20Materials/materials.ts
+ */
+export const MaterialPresets: Record<string, GeometryMaterialJSON> = {
+	FixedConcrete: {
+		shader: "InterscopeConcrete",
+		shaderKeywords: [
+			"DIFFUSE",
+			"DIRT",
+			"ENABLE_DIFFUSE",
+			"ENABLE_DIRT",
+			"ENABLE_DIRT_DETAIL",
+			"ENABLE_FOG",
+			"ENABLE_GROUND_FADE",
+			"ENABLE_SPECULAR",
+			"ENABLE_VERTEXT_COLOR",
+			"FOG",
+			"NOISE_DITHERING",
+			"REFLECTION_PROBE",
+			"REFLECTION_PROBE_BOX_PROJECTION",
+			"REFLECTION_PROBE_BOX_PROJECTION_OFFSET",
+			"_EMISSION",
+			"_ENABLE_FOG_TINT",
+			"_RIMLIGHT_NONE"
+		]
+	},
+	Dirt: {
+		shader: "InterscopeConcrete",
+		color: [0.5, 0.368, 0.36, 1],
+		shaderKeywords: ["DIFFUSE", "DIRT", "FOG", "REFLECTION_PROBE_BOX_PROJECTION"]
+	},
+	BlurryMetal: {
+		shader: "InterscopeConcrete",
+		shaderKeywords: ["DIRT", "DIFFUSE", "FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION", "REFLECTION_PROBE_BOX_PROJECTION_OFFSET"]
+	},
+	ShinyMetal: {
+		shader: "InterscopeCar",
+		shaderKeywords: ["DIRT", "DIFFUSE", "FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION", "REFLECTION_PROBE_BOX_PROJECTION_OFFSET"]
+	},
+	ShinyMetal2D: {
+		shader: "InterscopeCar",
+		shaderKeywords: ["DIRT", "DIFFUSE", "FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION_OFFSET"]
+	},
+	Glass: {
+		shader: "TransparentLight",
+		shaderKeywords: ["REFLECTION_PROBE"]
+	},
+	Grid: {
+		shader: "WaterfallMirror",
+		shaderKeywords: ["ENABLE_DIRT"]
+	},
+	FogLight: {
+		shader: "OpaqueLight",
+		shaderKeywords: ["ENABLE_HEIGHT_FOG"]
+	},
+	Invisible: {
+		shader: "InterscopeConcrete",
+		shaderKeywords: ["INSTANCING_ON"]
+	},
+	SlightReflection: {
+		shader: "BTSPillar",
+		shaderKeywords: ["DIFFUSE", "FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION"]
+	},
+	SlightReflectionNoColor: {
+		shader: "BTSPillar",
+		shaderKeywords: ["FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION"]
+	},
+	BlurryMirror: {
+		shader: "InterscopeConcrete",
+		shaderKeywords: ["DIFFUSE", "FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION"]
+	},
+	BlurryMirror2D: {
+		shader: "InterscopeConcrete",
+		shaderKeywords: ["DIFFUSE", "FOG", "REFLECTION_PROBE"]
+	},
+	ShinyMirror: {
+		shader: "InterscopeCar",
+		shaderKeywords: ["DIFFUSE", "FOG", "REFLECTION_PROBE", "REFLECTION_PROBE_BOX_PROJECTION"]
+	},
+	ShinyMirror2D: {
+		shader: "InterscopeCar",
+		shaderKeywords: ["DIFFUSE", "FOG", "REFLECTION_PROBE"]
+	}
+};
